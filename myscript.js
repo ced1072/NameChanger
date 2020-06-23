ec9ddfb08ac1f778f501b2562a6d66859f2c0502
ced1072 <52981096+ced1072@users.noreply.github.com>
2020-06-23 12:43:38 -0500

    added files via upload

---
+++ myscript.js
@@ -0,0 +1,46 @@
+// btn onclick example
+// function change()
+// {
+//   document.getElementById('button').className = "btn btn-danger";
+// }
+
+
+// make name big
+function changeSize()
+{
+  document.getElementById('name').style.fontSize = "100px"
+}
+
+// change color
+function changeColor()
+{
+  document.getElementById('name').style.color = "#9FA0C3"
+}
+
+// make capital
+function makeUppercase()
+{
+  document.getElementById('name').style.textTransform = "uppercase"
+}
+
+// hide
+function hide()
+{
+  document.getElementById('name').style.visibility = "hidden";
+}
+
+// show
+
+function show()
+{
+  document.getElementById('name').style.visibility = "visible";
+}
+
+// reset
+
+function reset() 
+{
+window.location.reload();
+}
+
+
